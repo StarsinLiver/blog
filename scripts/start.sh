82f7487e1de1f54315dc5290781519024e817c5b
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2023-12-27 15:43:03 +0900

    d

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "애플리케이션을 시작합니다."
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-exec > >(tee /var/log/codedeploy_stop.log|logger -t codedeploy_stop -s 2>/dev/console) 2>&1
+#!/usr/bin/env bash
 echo "애플리케이션을 중지합니다."
