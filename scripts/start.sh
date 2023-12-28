64f34084e847b916e5ee48b10c22d1c13b835747
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2023-12-28 13:10:14 +0900

    test 1

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@
 # exec > >(tee /var/log/codedeploy_start.log|logger -t codedeploy_start -s 2>/dev/console) 2>&1
 echo "애플리케이션을 시작합니다."
 # 여기에 애플리케이션 시작 명령어 또는 작업을 추가하세요.
-
+sudo systemctl start nginx
