e444ff75f325ebc7c318919be1913d2e26b35c95
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2023-12-29 01:03:06 +0900

    change ec2

--- src/utils/http-common.ts
+++ src/utils/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://52.78.132.72:8000/api",
+  baseURL: "http://15.165.77.140:8000/api",
   // baseURL: "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
