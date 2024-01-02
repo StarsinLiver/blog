76d87112dc932e2dbea134fca58ce4de1b27c0e2
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2024-01-02 14:41:37 +0900

    update

--- src/utils/http-common.ts
+++ src/utils/http-common.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: "http://15.165.77.140:8000/api",
-  // baseURL: "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   }
